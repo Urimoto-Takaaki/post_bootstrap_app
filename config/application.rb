751e9d5a50ba2b930c2ba61235969536d3d015e1
Urimoto-Takaaki <69062224+Urimoto-Takaaki@users.noreply.github.com>
2020-09-09 10:22:48 +0900

    管理者機能実装

--- config/application.rb
+++ config/application.rb
@@ -23,13 +23,8 @@ module PostBootstrapApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
+    # 以下の表記で管理画面を日本語化することができる
+    config.i18n.default_locale = :ja
     config.generators.system_tests = nil
   end
 end
